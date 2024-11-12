a48693b8e52e81b79fefc8ec38f9b0cb5fe96218
weiyun <2758670246@qq.com>
2024-11-12 15:14:58 +0800

    HelloWorld.java add printMessage method.

--- com/weiyun/HelloWorld.java
+++ com/weiyun/HelloWorld.java
@@ -8,4 +8,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HELLOWORLD");
     }
+    public void printMessage(String message){
+        System.out.println(message);
+    }
 }
