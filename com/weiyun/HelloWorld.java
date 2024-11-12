b38f801f14fb915ee9ea0a9e8b56cac82dbcb43e
weiyun <2758670246@qq.com>
2024-11-12 15:01:06 +0800

    添加.ignore文件，新增HelloWorld.java文件

---
+++ com/weiyun/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.weiyun;
+
+/***
+ *   @Author:weiyun
+ *
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HELLOWORLD");
+    }
+}
